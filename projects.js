2b26bb7e47541915cc078c5af05ebb477a6c712d
Rajivee J <jayarajivee@gmail.com>
2022-08-23 18:38:21 +0530

    first commit

---
+++ projects.js
@@ -0,0 +1,46 @@
+const projects = [{
+        img: "calc.png",
+        name: "Calculator",
+        techstack: ['html5', 'css-3'],
+        deploymentUrl: "https://rajivee-calculator.netlify.app/",
+        description: `A single page application for calculating sum,difference,multiplication and division of numbers.`
+    },
+    {
+        img: "form.png",
+        name: "Survey Form",
+        techstack: ['html5', 'css-3'],
+        deploymentUrl: "https://raji-events-survey-project.netlify.app/",
+        description: `An application for collecting the feedback from users on what they experience about the products of the company by providing their details and ratings.`
+    },
+    {
+        img: "events.jpg",
+        name: "Book for an event",
+        techstack: ['html5', 'css-3'],
+        deploymentUrl: "https://raji-events-survey-project.netlify.app/",
+        description: `A single page application used for booking  an event according to the running events and availability of seats.`
+    },
+    {
+        img: "callback.png",
+        name: "Callback hell",
+        techstack: ['html5', 'css-3', 'bootstrap-4'],
+        deploymentUrl: "https://rajivee-callbackhell.netlify.app/",
+        description: `A small callback function used to countdown and say some wishes.`
+    },
+    {
+        img: "youtube.png",
+        name: "Youtube Clone",
+        techstack: ['html5', 'css-3', 'bootstrap-4'],
+        deploymentUrl: "https://rajivee-youtubeclone.netlify.app/",
+        description: `A web application used to clone the main website of youtube. Some of its features are added like creating and uploading videos and searching for a videos.`
+    },
+    {
+        img: "cart.jpg",
+        name: "Add to Cart",
+        techstack: ['html5', 'css-3'],
+        deploymentUrl: "https://rajivee-addtocart.netlify.app/",
+        description: `A basic web application used to find our needed products and add to the cart.`
+    }
+    
+]
+
+//module.exports.projects = projects;
